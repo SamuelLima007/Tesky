d036c7038e870260aa0c63b3b07b43b7d54105ea
Samuel <samuelimagg@gmail.com>
2025-12-21 18:02:05 -0300

    Primeiro commit

--- To-Do-List-Backend/Program.cs
+++ To-Do-List-Backend/Program.cs
@@ -4,37 +4,37 @@ using To_Do_List_Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
-builder.Services.AddOpenApi(); // Swagger
+
+builder.Services.AddOpenApi(); 
 builder.Services.AddDbContext<TaskDataContext>(options =>
     options.UseSqlite("Data Source=Tasks.db"));
 builder.Services.AddControllers();
 var generator = new IdGenerator(0);
 builder.Services.AddSingleton(generator);
 
-// Configurar CORS corretamente
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAnyOriginPolicy",
         policy =>
         {
-            policy.AllowAnyOrigin()   // Permite qualquer origem (domínio)
-                  .AllowAnyMethod()   // Permite GET, POST, PUT, DELETE etc.
-                  .AllowAnyHeader();  // Permite qualquer cabeçalho
+            policy.AllowAnyOrigin()   
+                  .AllowAnyMethod()  
+                  .AllowAnyHeader(); 
         });
 });
 
 var app = builder.Build();
 
-// Middleware: ordem correta
-app.UseCors("AllowAnyOriginPolicy"); // 🔹 Ativa CORS antes de qualquer requisição
+
+app.UseCors("AllowAnyOriginPolicy"); 
 app.UseHttpsRedirection();
 
 app.MapControllers();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi(); // Swagger para testar a API
+    app.MapOpenApi(); 
 }
 
 app.Run();

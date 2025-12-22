56fc72d0dfbf3076179be4ba2beeedddfe7fd4da
Samuel <samuelimagg@gmail.com>
2025-12-21 23:40:24 -0300

    Projeto Tesky

--- To-Do-List-Backend/obj/Debug/net10.0/To-Do-List-Backend.AssemblyInfo.cs
+++ To-Do-List-Backend/obj/Debug/net10.0/To-Do-List-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do-List-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e2a870f8141a2ed99b15d465273920bcd694c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do-List-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-Do-List-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

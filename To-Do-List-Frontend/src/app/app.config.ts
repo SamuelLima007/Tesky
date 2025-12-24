43fb7b5457b5afb0bdf5afb64117af8e1e6d393b
Samuel Lima da Silva <samuel.lsilva@grupomateus.com>
2025-12-24 15:20:05 -0300

    Atualizacoes

--- To-Do-List-Frontend/src/app/app.config.ts
+++ To-Do-List-Frontend/src/app/app.config.ts
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
      providePrimeNG({
             theme: {
-                preset: Aura
+                preset: Aura,
             }
         })
   ]
--- To-Do-List-Frontend/src/app/pages/main/main.ts
+++ To-Do-List-Frontend/src/app/pages/main/main.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [SelectButtonModule, FormsModule],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
 export class Main {
+
+  stateOptions = [
+    {
+      label: "Total", value: "a"
+
+    },
+    {
+       label: "Concluidas", value: "b"
+    },
+    {
+       label: "Ativas", value: "c"
+    }
+  ]
+
+  value : string = "";
 
 }
